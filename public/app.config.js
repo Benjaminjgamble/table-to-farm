f27d5569d5922d6bd6c6cd8d2bc188617e411a2f
Benjamin Gamble <Benjaminjgamble@me.com>
2017-07-03 12:36:10 -0700

    initial commit

---
+++ public/app.config.js
@@ -0,0 +1,13 @@
+(function() {
+  angular.module('app').config(config)
+  config.$inject = ['$stateProvider', '$locationProvider']
+  function config($stateProvider, $locationProvider){
+    $locationProvider.html5Mode(true)
+    $stateProvider
+
+    .state({
+      name: 'home', url: '/', redirectTo: 'landingIndex'
+    })
+    
+  }
+}())
---
+++ public/app.module.js
@@ -0,0 +1,6 @@
+(function(){
+  'use strict'
+  angular.module('app', ['ui.router'])
+  // .constant('API_BASE_URL', '<heroku-url>')
+  .constant('API_BASE_URL', 'http://localhost:3000')
+})()
