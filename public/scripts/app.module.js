0f524cf46c9faf2a49793f46f5b289f8173d1f1a
Benjamin Gamble <Benjaminjgamble@me.com>
2017-07-20 11:04:18 -0700

    app.module

--- public/scripts/app.module.js
+++ public/scripts/app.module.js
@@ -1,6 +1,6 @@
 (function(){
   'use strict'
   angular.module('app', ['ui.router', 'angularMoment'])
-  .constant('API_BASE_URL', 'https://agile-thicket-46062.herokuapp.com/')
-  // .constant('API_BASE_URL', 'http://localhost:3000')
+  // .constant('API_BASE_URL', 'https://agile-thicket-46062.herokuapp.com/')
+  .constant('API_BASE_URL', 'http://localhost:3000')
 })()
